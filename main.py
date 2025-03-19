4dde26ec867fdd406f8e0144c0a096622a910c26
KrzysztofMastuszewski <krzysiek.matuszewski.pl@gmail.com>
2025-03-19 14:15:44 +0100

    init

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def main():
-    print("Hello, World!")
+# app.py
+from flask import Flask, render_template
 
-if __name__ == "__main__":
-    main()
+app = Flask(__name__)
 
-    
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
